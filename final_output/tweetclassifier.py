6369d40f2b3bac8b428b9bc19f162cb112b0aa55
RHammond2 <reh388@nyu.edu>
2017-12-10 17:14:14 -0500

    updating class

--- final_output/tweetclassifier.py
+++ final_output/tweetclassifier.py
@@ -3,127 +3,231 @@ import sys
 import string
 import pickle
 import tarfile
-from gensim.models import ldamodel, Phrases, phrases
+import numpy as np
+import pandas as pd
 from gensim import corpora
+from gensim.models import ldamodel, Phrases, phrases
+from nltk import bigrams
 from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from nltk.stem.snowball import SnowballStemmer
+
 
 class classifytweet:
-    def __intit__(self, tweet_body='tweet_body'):
+    def __init__(self, model_files='model_files/'):
         """
         tweet_body: the individual tweet
         model_files: the location for the model files. leave the model files folder as is
                      unless you have changed the location of all the model files.
         """
-        self.tweet = tweet_body
-        self.model = None
-        self.corpus = None
-        self.dictionary = None
-
-    def initialize(self):
-        """
-        Initialize the model.
-        """
-        self.model, self.corpus, self.dictionary, self.phraser = load_model()
-        self.lemmatized, self.stemmed, self.phrased = prepare_tweet()
-
-    def load_model(self, model_files='model_files/'):
+        self.model_files = model_files
+        #self.model, self.corpus, self.dictionary, self.phraser, self.valence_mean, self.arousal_mean, self.valence_sd, self.arousal_sd = self.load_model()
+        self.load_model()
+        self.n = len(self.dictionary.items())
+        self.word_map = {v:k for k,v in self.dictionary.items()}
+        print("Model items loaded and classifier initialized!")
+        
+    def load_model(self):
         """
         Loads the model, corpus, and dictionary.
         """
-        # extract tarfile
-        for f in os.listdir(model_files):
+        # extract tarfiles
+        for f in os.listdir(self.model_files):
             if f.endswith('.gz'):
-                tar = tarfile.open(model_files + f, "r:gz")
-                tar.extractall(path=model_files)
+                tar = tarfile.open(self.model_files + f, "r:gz")
+                tar.extractall(path=self.model_files)
                 tar.close()
 
         # load model, corpus, and dictionary objects
-        fnames = [fn for fn in os.listdir(model_files) if '.gensim' in fn]
-        model = ldamodel.LdaModel.load(model_files + fnames[0])
-        corpus = corpora.MmCorpus(model_files + 'unigrams_corpus.mm')
-        dictionary = corpora.Dictionary.load(model_files + 'unigrams_dictionary.pkl')
-        model.id2word = dictionary
-        phraser = phrases.load(model_files+'document_phraser.pkl')
+        fnames = [fn for fn in os.listdir(self.model_files) if '.gensim' in fn]
+        self.model = ldamodel.LdaModel.load(self.model_files + fnames[0])
+        self.corpus = corpora.MmCorpus(self.model_files + 'unigrams_corpus.mm')
+        self.dictionary = corpora.Dictionary.load(self.model_files + 'unigrams_dictionary.pkl')
+        self.model.id2word = self.dictionary
+        self.phraser = phrases.Phrases.load(self.model_files + 'document_phraser.pkl')
+        for f in ['unigrams_dictionary.pkl', 'unigrams_corpus.mm', 'unigrams_corpus.mm.index', 'NB_vectorizer.pkl', 'NB_sentiment_model.pkl']:
+            fnames.append(f)
+        
+        # load the valence and arousal arrays
+        self.valence_mean = pickle.load(open(self.model_files + 'valence_mean.pkl', 'rb'))
+        self.arousal_mean = pickle.load(open(self.model_files + 'arousal_mean.pkl', 'rb'))
+        self.valence_sd = pickle.load(open(self.model_files + 'valence_sd.pkl', 'rb'))
+        self.arousal_sd = pickle.load(open(self.model_files + 'arousal_sd.pkl', 'rb'))
+        
+        # load the MinMaxScaler for the transforming the scores
+        self.base_outrage_scaler = None
+        self.expanded_outrage_scaler = None
+        self.valence_scaler = None
+        self.arousal_scaler = None
+        self.emoji_scaler = None
+        self.topic_valence_scaler = pickle.load(open(self.model_files + 'topic_valence_scaled.pkl', 'rb'))
+        self.topic_arousal_scaler = pickle.load(open(self.model_files + 'topic_arousal_scaled.pkl', 'rb'))
+        
+        # load the Naive Bayes sentiment model
+        try:
+            self.nb_model = pickle.load(open(self.model_files + 'NB_sentiment_model.pkl', 'rb'))
+        except:
+            self.nb_model = pickle.load(open(self.model_files + 'NB_sentiment_model.pkl', 'rb'), encoding='latin1')
+        try:
+            self.nb_vectorizer = pickle.load(open(self.model_files + 'NB_vectorizer.pkl', 'rb'))
+        except:
+            self.nb_vectorizer = pickle.load(open(self.model_files + 'NB_vectorizer.pkl', 'rb'), encoding='latin1')
+        
+        # load the outrage dictionaries
+        self.outrage_list = pd.read_csv(self.model_files + 'outrage_dictionary_stemmed.csv', header=None)
+        self.exp_outrage_list = pd.read_csv(self.model_files + 'expanded_outrage_dictionary_stemmed.csv', header=None)
 
-        #cleanup the unzipped files
+        # cleanup the unzipped files
         for f in fnames:
-            os.remove(f)
-        return model, corpus, dictionary, phraser
+            os.remove(self.model_files + f)
+            
+        return "All modeling information loaded"
 
-    def prepare_tweet(self):
+    def prepare_tweet(self, tweet):
         """
         Turn that unstructured text into sweet, sweet, "cleaned" up tokens!
         """
-
-        lemmatized = # TO DO
-
-        stemmed = # TO DO
+        self.tweet = tweet
+        stemmer = SnowballStemmer("english")
+        tokenizer = TweetTokenizer()
+        self.tweet_tokenized = tokenizer.tokenize(self.tweet)
+        try:
+            self.tweet_tokenized = [unicode(y.encode("utf-8"), errors='ignore') for y in self.tweet_tokenized]
+            self.stemmed = [stemmer.stem(y) for y in self.tweet_tokenized]
+        except:
+            #tweet_tokenized = [y.encode("utf-8") for y in tweet_tokenized]
+            self.stemmed = [stemmer.stem(y) for y in self.tweet_tokenized]
+        #self.stemmed = filter('', self.stemmed)
+        #self.processed = ''.join(self.stemmed)
+        #self.text_bigrams = [' '.join(self.stemmed[i:]) for i in range(2)]
+        self.text_bigrams=list(bigrams(self.stemmed))
+        self.text_bigrams=["%s %s" % x for x in self.text_bigrams]
+        self.text_bigrams.extend(self.stemmed)
 
         keep = set(['!','?'])
         stop = set(stopwords.words('english'))
         remove = set([x for x in list(string.punctuation) if x not in keep])
         stop.update(remove)
         stop.update(['',' ','  '])
-        stemmed = [d for d in self.tweet.split(" ") if d not in stop] # NEEDS TO UPDATED FOR PORTER STEMMER-ING THE TWEET
-        phrased = self.phraser([stemmed])
+        stemmed = [d for d in self.stemmed if d not in stop]
+        self.phrased = list(self.phraser[[stemmed]])[0]
 
-    return lemmatized, stemmed, phrased
-
-    def valence_arousal(self):
+        print('Phrased representation: "' + ' '.join(self.phrased) + '"')
+        #return None
+    
+    def get_valence_score(self):
         """
         Creates the valence and arousal score for the tweet.
         """
+        tweet_arr = np.zeros(self.n)
+        for word in set(self.phrased) & set(self.word_map.keys()):
+            tweet_arr[self.word_map[word]] = 1.
+        mean = tweet_arr * self.valence_mean
+        sd = tweet_arr * self.valence_sd
+        total_sd = np.sum(sd) * tweet_arr
+        with np.errstate(divide='ignore'):
+            sd_ratio = total_sd / sd
+            sd_ratio[sd == 0] = 0
+        sd_weight = sd_ratio / np.sum(sd_ratio)
+        
+        self.valence_score = np.sum(mean*sd_weight)
+        
+        return self.valence_score
 
-        return valence_score, arousal_score
+    def get_arousal_score(self):
+        """
+        Creates the valence and arousal score for the tweet.
+        """
+        tweet_arr = np.zeros(self.n)
+        for word in set(self.phrased) & set(self.word_map.keys()):
+            tweet_arr[self.word_map[word]] = 1.
+        mean = tweet_arr * self.arousal_mean
+        sd = tweet_arr * self.arousal_sd
+        total_sd = np.sum(sd) * tweet_arr
+        with np.errstate(divide='ignore'):
+            sd_ratio = total_sd / sd
+            sd_ratio[sd == 0] = 0
+        sd_weight = sd_ratio / np.sum(sd_ratio)
+        
+        self.arousal_score = np.sum(mean*sd_weight)
+        
+        return self.arousal_score
 
-    def sentiment_score(self):
+    def get_sentiment_score(self):
         """
         Weights the posititive/negative sentiment of the tweet.
         """
+        vectorized = self.nb_vectorizer.transform(self.text_bigrams)
+        self.sentiment_score = np.average(1 - self.nb_model.predict_proba(vectorized)[:,1])
 
-        return sentiment_score
+        return self.sentiment_score
 
     def get_topics(self):
         """
         Extract the topics from the tweet using the LDA model.
         """
+        return self.model.get_document_topics(self.model.id2word.doc2bow(self.phrased), per_word_topics=False)
 
-        return topic_list_of_tuples
-
-    def emoji_cout(self):
+    def get_emoji_count(self):
         """
         Count the Mad! faces.
         """
+        positives = ['<U+0082>', '<U+008D>']
+        outrage = ['<U+00A0>', '<U+00A1>', '<U+00A4>', '<U+00A9>']
+        positive_score = sum([y in positives for y in self.tweet_tokenized])
+        outrage_score = sum([y in outrage for y in self.tweet_tokenized])
+        self.emoji_count = outrage_score-positive_score
+        return self.emoji_count
 
-        return emoji_count
-
-    def outrage_count(self):
+    def get_base_outrage_count(self):
         """
         Get the number of outrage words in the tweet.
         """
+        self.base_outrage_count = len(set(self.stemmed) & set(self.outrage_list))
+        return self.base_outrage_count
 
-        return outrageousness
+    def get_expanded_outrage_count(self):
+        """
+        Get the number of outrage words in the tweet.
+        """
+        outrage = set(self.stemmed) & set(self.exp_outrage_list)
+        self.expanded_outrage_count = 0
+        for i in self.stemmed:
+            if i in (self.exp_outrage_list):
+                self.expanded_outrage_count += 1
 
+        return self.expanded_outrage_count
 
-    def outrage_score(self):
+    def get_outrage_score(self):
         """
         Uses the results of each of the index measures to create one score.
+        .20 outrage dict
+        .15 expanded outrage dict
+        .15 valence
+        .13 arousal
+        .11 sentiment
+        .10 emoji
+        .08 topic valence
+        .08 topic arousal
         """
-        self.va = valence_arousal()
-        self.sentiment = sentiment_score()
-        self.topics = get_topics()
-        self.emjois = emoji_count()
-        self.outrageousness = outrage_count()
+        self.topics = self.get_topics()
+        topic_valence_score = 0
+        topic_arousal_score = 0
+        for tup in self.topics:
+            topic_valence_score += self.topic_valence_scaler[tup[0]] * tup[1]
+            topic_arousal_score += self.topic_valence_scaler[tup[0]] * tup[1]
+            
+        scores = np.array([
+            self.get_base_outrage_count(),
+            self.get_expanded_outrage_count(),
+            self.get_valence_score(),
+            self.get_arousal_score(),
+            self.get_sentiment_score(),
+            self.get_emoji_count(),
+            topic_valence_score,
+            topic_arousal_score
+            ])
+        weights = np.array([0.2, 0.15, 0.15, 0.13, 0.11, 0.10, 0.08, 0.08])
 
-        outrageous_meter = # DO SOME SHIT!
-
-        return outrageous_meter
-
-
-
-
-if __name__ == '__main__':
-
-    tweet_body = sys.argv[1] # the body of the tweet
-    classifier = classifytweet(tweet_body)
-    classifier.initialize()
+        self.outrage_meter = np.sum(scores*weights)
+        return self.outrage_meter
